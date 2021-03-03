e96e4f88c287ab10b4aa30f7efd7c86e0509f8f3
Griss <37448271+KGriss@users.noreply.github.com>
2021-03-03 15:18:03 +0100

    Auto update result while changing mode
    + better error log

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-document.getElementById("crypt").addEventListener("input", encrypt);
-document.getElementById("mode").addEventListener("click", mode);
+let cryptInput = document.getElementById("crypt");
+cryptInput.addEventListener("input", encrypt)
+
+let modeButton = document.getElementById("mode");
+modeButton.addEventListener("click", mode);
 
 let cryptMode = 1;
 
@@ -10,22 +13,23 @@ function mode(e) {
 	cryptMode++;
 	switch (cryptMode) {
 		case 2:
-			document.getElementById("mode").textContent = "NORMAL";
+			modeButton.textContent = "NORMAL";
 			break;
 		case 3:
-			document.getElementById("mode").textContent = "HARD";
+			modeButton.textContent = "HARD";
 			break;
 		case 4:
 			cryptMode = 1;
-			document.getElementById("mode").textContent = "EASY";
+			modeButton.textContent = "EASY";
 			break;
 		default:
-			console.log("ERROR : function \"mode\" from #9 to #31");
+			console.log("ERROR : wrong value of \"cryptMode\" in function \"mode\" line #11");
 	}
+	encrypt();
 }
 
 function encrypt(e) {
-	let msg = e.target.value;
+	let msg = cryptInput.value;
 	let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz²&é\"'(-è_çà)=^$ù*,;:!°+¨£%µ?./§<>~#{[|`@]}€¤\\êï0123456789";
 	let easyLeet = "48CD3FGH1JKLMN0PQRSTUVWXYZ 48cd3fgh1jklmn0pqrstuvwxyz²&é\"'(-è_çà)=^$ù*,;:!°+¨£%µ?./§<>~#{[|`@]}€¤\\êïOI2EA567B9";
 	let normalLeet = "48CD3F6H1JKLMN0PQR5TUVWXYZ 48cd3f6h1jklmn0pqr5tuvwxyz²&é\"'(-è_çà)=^$ù*,;:!°+¨£%µ?./§<>~#{[|`@]}€¤\\êïOI2EASG7B9";
